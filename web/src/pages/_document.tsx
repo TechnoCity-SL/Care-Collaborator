94eeb302fa8396f488c87ea99c85e4e157f11e30
ShaneDushyantha <87410956+ShaneDushyantha@users.noreply.github.com>
2026-07-21 17:27:24 +0530

    Added Fav icon

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -4,7 +4,13 @@ import { fontVariables } from "@/lib/fonts";
 export default function Document() {
   return (
     <Html lang="en" className={fontVariables}>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
